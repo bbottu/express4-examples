34d9cba771d3679395e9ddcb264f2346bcf2b8ff
Bart Bottu <bbottu@gmail.com>
2015-01-20 23:15:08 +0100

    Updated config

--- Protect API with JSON Web Token/config.js
+++ Protect API with JSON Web Token/config.js
@@ -4,7 +4,7 @@ module.exports = {
     password: 'password'                              //Password to log in with
   },
   token: {
-    secret: 'miart-app',                              //The secret for generating the token
+    secret: 'blabla',                              //The secret for generating the token
     expiresInMinutes: 10,                             //Time after which the token expires - refresh token before it expires
     maxExpiresInMinutes: 180                          //Time after which the token can no longer be refreshed
   }
